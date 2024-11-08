d2882110b0fa3f53db4d723de36d124fce878588
Avichai Gal_Or <avichai504@gmail.com>
2024-11-08 13:04:02 +0200

    reset app

---
+++ src/app/chart/page.tsx
@@ -0,0 +1,3 @@
+export default function Chart(){
+  return <div className="bg-red-400 p-4" >Chart</div>
+}
---
+++ src/components/Hello.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+const Hello = () => {
+  return <div className='bg-red-600 p-4' >Hello</div>
+}
+
+export default Hello
