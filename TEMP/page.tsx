e964c75bcc25b15895264cde12933f59109c4703
Avichai Gal_Or <avichai504@gmail.com>
2024-11-21 12:47:32 +0200

    example single QR History

---
+++ TEMP/page.tsx
@@ -0,0 +1,15 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1 className='text-xl font-bold text-center underline'>
+        היסטורית אירועים עבור QR מספר {5555}
+      </h1>
+      {/* <HistoryQr data={data} */}
+      {/* prob */}
+      {/* bgtReq */}
+      {/* waiting */}
+      {/* skipped */}
+      {/* completed */}
+    </div>
+  )
+}
